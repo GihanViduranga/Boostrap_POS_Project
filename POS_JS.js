06ce2edce07fdc9398a0b52b7f7fa495ce904b03
gihanviduranga <gihanvidu123@gmai.com>
2024-10-22 16:27:57 +0530

    Done the Item Updating and Item table

--- POS_JS.js
+++ POS_JS.js
@@ -54,4 +54,87 @@ Order_History.addEventListener("click", function (){
     Store_sec.style.display = "none";
     Order_sec.style.display = "none";
     OrderHistory.style.display = "block";
-})
+});
+
+///////////////////////////////////////////////////////
+/*Customer Save & Table update*/
+//////////////////////////////////////////////////////
+
+let CustomerDB = [];
+
+$("#SaveCustomer").on("click", function() {
+    let First_Name = $("#firstName").val();
+    let Last_Name = $("#lastName").val();
+    let Email = $("#email").val();
+    let Phone_Number = $("#mobile").val();
+    let Cus_Address = $("#address").val();
+
+    console.log()
+
+    let customerData = {
+        id : CustomerDB.length + 1,
+        FirstName : First_Name,
+        LastName : Last_Name,
+        CusEmail : Email,
+        PhoneNumber : Phone_Number,
+        CustomerAddress : Cus_Address
+    }
+
+
+    CustomerDB.push(customerData);
+
+    customerTable();
+});
+
+const customerTable = () => {
+    $("#CustomerTable").empty();
+    CustomerDB.map((item,index) => {
+        let Data = `<tr>
+            <td>${item.id}</td>
+            <td>${item.FirstName}</td>
+            <td>${item.LastName}</td>
+            <td>${item.CusEmail}</td>
+            <td>${item.CustomerAddress}</td>
+            <td>${item.PhoneNumber}</td>
+            </tr>`
+        $("#CustomerTable").append(Data);
+    });
+}
+
+///////////////////////////////////////////////////////
+/*Item Save & Table update*/
+//////////////////////////////////////////////////////
+
+let ItemDB = [];
+
+$("#SaveItem").on("click", function() {
+    let Item_Name = $("#itemName").val();
+    let Item_Price = $("#price").val();
+    let Item_Quantity = $("#qty").val();
+
+    console.log()
+
+    let itemData = {
+        id : ItemDB.length + 1,
+        ItemName : Item_Name,
+        ItemPrice : Item_Price,
+        ItemQuantity : Item_Quantity
+    }
+
+    ItemDB.push(itemData);
+    itemTable();
+});
+
+const itemTable = () => {
+    $("#ItemTable").empty();
+    ItemDB.map((item,index) => {
+        let Data = `<tr>
+            <td>${item.id}</td>
+            <td>${item.ItemName}</td>
+            <td>${item.ItemQuantity}</td>
+            <td>${item.ItemPrice}</td>
+            </tr>`
+        $("#ItemTable").append(Data);
+    });
+}
+
